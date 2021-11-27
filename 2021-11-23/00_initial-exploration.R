d623ddeccda0f065182234fdc2f19f649e440368
jakepscott <jakepscott16@gmail.com>
2021-11-26 21:14:37 -0500

    Beautify plot

--- 2021-11-23/00_initial-exploration.R
+++ 2021-11-23/00_initial-exploration.R
@@ -6,6 +6,8 @@ library(showtext)
 library(here)
 #font_add_google("Roboto", "roboto")
 showtext_auto()
+showtext_opts(dpi = 600)
+
 # Load Data ---------------------------------------------------------------
 directors <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-11-23/directors.csv')
 episodes <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-11-23/episodes.csv')
@@ -29,23 +31,34 @@ top_words <- imdb %>%
 top_words %>% 
   head(10) %>% 
   ggplot(aes(fct_reorder(word, percent), percent)) +
-  geom_col(color = "#3D24FF",
-           fill = "#3D24FF") +
-  scale_y_continuous(labels = function(y) glue("{y}%")) +
-  coord_flip() +
+  geom_col(color = "#4F90FF",
+           fill = "#4F90FF") +
+  scale_y_continuous(labels = function(y) glue("{y}%"),
+                     expand = expansion(c(0,0.01))) +
+  #coord_cartesian() +
+  coord_flip(ylim = c(0,8)) +
   labs(x = NULL,
        y = "Percent of Non-Stop Words in IMDB Episode Descriptions",
-       title = "It seems the Doctor is often dealing with problems of time and friends on Earth, using Tardis",
-       subtitle = "As someone who has never seen a single episode, I was hoping the most common words in the IMDB descriptions would give me some insight",
+       title = "It seems the Doctor is often dealing with problems of time \nand friends on Earth, using Tardis",
+       subtitle = "As someone who has never seen a single episode, I was hoping the most common words in \nthe IMDB descriptions would give me some insight",
        caption = "Plot: @jakepscott2020 | Data: Tidytuesday, Datardis Package") +
-  theme_minimal(base_size = 12, 
-                base_family = "roboto") +
+  # theme_minimal(base_size = 12, 
+  #               base_family = "roboto") +
   theme(plot.title.position = "plot", 
         plot.background = element_rect(fill = 'grey20'),
         panel.background = element_rect(fill  = 'grey20'),
         panel.grid = element_blank(), 
         axis.text = element_text(color = "white"),
         axis.title = element_text(color = "white"),
-        plot.title = element_text(color = 'white'),
-        plot.subtitle = element_text(color = "grey70"),
-        plot.caption = element_text(color = "grey70", face = "italic"))
+        plot.title = element_text(color = 'white',
+                                  size = rel(1.35),
+                                  face = "bold"),
+        plot.subtitle = element_text(color = "grey70", 
+                                     size = rel(.8),
+                                     face = "italic"),
+        plot.caption = element_text(color = "grey70", 
+                                    face = "italic",
+                                    size = rel(0.74)))
+
+ggsave(here("2021-11-23/figures/top-words.png"), dpi = 600,
+       height =4, width = 4.*1.62, units = "in")
